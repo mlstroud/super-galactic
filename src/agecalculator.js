3df5f007dedac1ea260c9a8151239bbb661c0646
Matt Stroud <lixingke06@gmail.com>
2020-04-17 10:19:44 -0700

    initiate class variables to 0

--- src/agecalculator.js
+++ src/agecalculator.js
@@ -1,5 +1,12 @@
 export class AgeCalculator {
   constructor(userAge) {
     this.age = userAge;
+    this.ageOnMercury = 0;
+    this.ageOnVenus = 0;
+    this.ageOnMars = 0;
+    this.ageOnJupiter = 0;
+    this.lifeExpectancy = {};
+    this.yearsLeftToLive = {};
+    this.yearsSurpassed = {};
   }
 }
