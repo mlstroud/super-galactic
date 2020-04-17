d2a877721b68c25c74cc3e5845097d680eb17e7c
Matt Stroud <lixingke06@gmail.com>
2020-04-17 09:42:36 -0700

    create test for user's age to fail

--- __tests__/agecalculator.test.js
+++ __tests__/agecalculator.test.js
@@ -2,4 +2,14 @@ import { AgeCalculator } from './../src/agecalculator.js';
 
 describe('Age Calculator', () => {
 
+  let ageCalculator;
+
+  beforeEach(() => {
+    ageCalculator = new AgeCalculator(32);
+  });
+
+  test('should correctly create an AgeCalculator object and assign user age', () => {
+    expect(ageCalculator.age).toEqual(35);
+  });
+
 });
