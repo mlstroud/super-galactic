42a40e999ad0dc263fcecf336deae76c4ce9b1a3
Matt Stroud <lixingke06@gmail.com>
2020-04-17 16:00:25 -0700

    import css styles and jquery

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from "jquery";
 import { ageCalculator } from './agecalculator.js';
 
 $(document).ready(function() {
