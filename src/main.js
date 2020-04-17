664de637319e4f68d78be4f1e5ae112dd4645c07
Matt Stroud <lixingke06@gmail.com>
2020-04-17 16:54:26 -0700

    change code to only call event listeners

--- src/main.js
+++ src/main.js
@@ -2,21 +2,9 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import { getResultHTML } from './userinterfacelogic.js';
+import { attachListeners } from './userinterfacelogic.js';
 
 $(document).ready(function() {
 
-  $("#form-age").submit(function(event) {
-    event.preventDefault();
-
-    const name = $("#name").val();
-    const age  = parseInt($("#age").val());
-    let resultHTML = getResultHTML(age);
-    let message = `Hi <strong>${name}</strong>, this is a detailed breakdown of your age throughout the solar system!<br><br>`;
-    
-    $("#content-form").slideUp();
-    $("#content-results").prepend(message);
-    $("#table-result").append(resultHTML);
-    $("#content-results").slideDown("slow");
-  });
+  attachListeners();
 });
